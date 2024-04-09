d1964e87812bfb348baa63945c47cdeed459e15e
Tirivashe <shaymusts@gmail.com>
2024-04-09 09:33:58 +0200

    update to have role in user creation

--- apps/server/src/user/dto/create-user.dto.ts
+++ apps/server/src/user/dto/create-user.dto.ts
@@ -1,5 +1,12 @@
+// TODO: add validation and extract enum to another file
+enum Role {
+  Admin = 'Admin',
+  Regular = 'Regular',
+}
+
 export class CreateUserDto {
   username: string;
   email: string;
   password: string;
+  role: Role;
 }
