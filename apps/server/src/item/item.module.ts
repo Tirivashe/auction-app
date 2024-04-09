5c2e0dd1bc047890207f8daf46014b09264b77d6
Tirivashe <shaymusts@gmail.com>
2024-04-09 22:02:32 +0200

    transaction for placing bid and creating a bid history

--- apps/server/src/item/item.module.ts
+++ apps/server/src/item/item.module.ts
@@ -4,12 +4,17 @@ import { ItemService } from './item.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Item, ItemSchema } from './schema/item.schema';
 import { Bid, BidSchema } from 'src/bid/schema/bid.schema';
+import {
+  BiddingHistory,
+  BiddingHistorySchema,
+} from 'src/bid/schema/bid-history.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Item.name, schema: ItemSchema },
       { name: Bid.name, schema: BidSchema },
+      { name: BiddingHistory.name, schema: BiddingHistorySchema },
     ]),
   ],
   controllers: [ItemController],
--- apps/server/src/item/item.service.ts
+++ apps/server/src/item/item.service.ts
@@ -1,18 +1,23 @@
 import { BadRequestException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
-import { InjectModel } from '@nestjs/mongoose';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
 import { Item } from './schema/item.schema';
-import { Model } from 'mongoose';
+import { Connection, Model } from 'mongoose';
 import { UpdateItemDto } from './dto/update-item.dto';
 import { QueryParamsDto } from './dto/query-params.dto';
 import { PlaceBidDto } from './dto/place-bid.dto';
 import { Bid } from 'src/bid/schema/bid.schema';
+import { BiddingHistory } from 'src/bid/schema/bid-history.schema';
+import { Status } from 'src/types';
 
 @Injectable()
 export class ItemService {
   constructor(
     @InjectModel(Item.name) private itemModel: Model<Item>,
     @InjectModel(Bid.name) private readonly bidModel: Model<Bid>,
+    @InjectModel(BiddingHistory.name)
+    private readonly biddingHistoryModel: Model<BiddingHistory>,
+    @InjectConnection() private connection: Connection,
   ) {}
   async getAllItems(queryParams: QueryParamsDto): Promise<Item[]> {
     const {
@@ -49,13 +54,8 @@ export class ItemService {
       placeBidDto,
     );
     if (!canBid) throw new BadRequestException(message);
-
-    const bid = await this.bidModel.create({
-      user: placeBidDto.userId,
-      item: itemId,
-      bidAmount: placeBidDto.amount,
-    });
-    return bid;
+    await this.createBid(itemId, placeBidDto);
+    return { message: 'Bid created', status: HttpStatus.CREATED };
   }
 
   async updateItem(updateItemDto: UpdateItemDto, id: string) {
@@ -76,23 +76,14 @@ export class ItemService {
     placeBidDto: PlaceBidDto,
   ) {
     if (allBidsForItem.length > 0) {
-      const highestBid = (
-        await this.bidModel
-          .find({ item: itemId })
-          .sort({
-            bidAmount: -1,
-          })
-          .limit(1)
-      )[0];
-
+      const highestBid = await this.getHighestBidForItem(itemId);
       if (highestBid.user._id.toString() === placeBidDto.userId) {
         return {
           canBid: false,
           message: 'You are currently the highest bidder on this item',
         };
       }
-
-      if (highestBid.bidAmount >= placeBidDto.amount) {
+      if (highestBid.bidAmount + 1 >= placeBidDto.amount) {
         return {
           canBid: false,
           message: 'Place a bid higher than the current highest bid amount',
@@ -106,5 +97,49 @@ export class ItemService {
         message: 'Place a bid higher than the current price',
       };
     return { canBid: true, message: '' };
+  }
+
+  private async getHighestBidForItem(itemId: string): Promise<Bid> {
+    return (
+      await this.bidModel
+        .find({ item: itemId })
+        .sort({
+          bidAmount: -1,
+        })
+        .limit(1)
+    )[0];
+  }
+
+  private async createBid(itemId: string, placeBidDto: PlaceBidDto) {
+    const session = await this.connection.startSession();
+    session.startTransaction();
+    try {
+      const existingBiddingHistory = await this.biddingHistoryModel.findOne({
+        item: itemId,
+        user: placeBidDto.userId,
+      });
+      const newBid: Bid = new this.bidModel({
+        user: placeBidDto.userId,
+        item: itemId,
+        bidAmount: placeBidDto.amount,
+      });
+      if (!existingBiddingHistory) {
+        const newBiddingHistory: BiddingHistory = new this.biddingHistoryModel({
+          bid: newBid._id,
+          user: placeBidDto.userId,
+          item: itemId,
+          bidStatus: Status.InProgress,
+          autobid: false,
+        });
+        await newBiddingHistory.save({ session });
+      }
+      await newBid.save({ session });
+      await session.commitTransaction();
+    } catch (err) {
+      await session.abortTransaction();
+      throw err;
+    } finally {
+      session.endSession();
+    }
   }
 }
