65a20fbcb1edb208265e1d0b5647825bac7eef9f
Tirivashe <shaymusts@gmail.com>
2024-04-09 10:28:55 +0200

    complete crud operations on items

--- apps/server/src/item/item.controller.ts
+++ apps/server/src/item/item.controller.ts
@@ -1,13 +1,17 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
+  Param,
+  Patch,
   Post,
 } from '@nestjs/common';
 import { ItemService } from './item.service';
 import { CreateItemDto } from './dto/create-item.dto';
+import { UpdateItemDto } from './dto/update-item.dto';
 
 @Controller()
 export class ItemController {
@@ -22,5 +26,20 @@ export class ItemController {
   @HttpCode(HttpStatus.CREATED)
   async createItem(@Body() createItemDto: CreateItemDto) {
     return await this.itemService.createItem(createItemDto);
+  }
+
+  @Patch('item/:id')
+  @HttpCode(HttpStatus.OK)
+  async updateItem(
+    @Body() updateItemDto: UpdateItemDto,
+    @Param('id') id: string,
+  ) {
+    return await this.itemService.updateItem(updateItemDto, id);
+  }
+
+  @Delete('item/:id')
+  @HttpCode(HttpStatus.OK)
+  async deleteItem(@Param('id') id: string) {
+    return await this.itemService.deleteItem(id);
   }
 }
--- apps/server/src/item/item.service.ts
+++ apps/server/src/item/item.service.ts
@@ -3,6 +3,7 @@ import { CreateItemDto } from './dto/create-item.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Item } from './schema/item.schema';
 import { Model } from 'mongoose';
+import { UpdateItemDto } from './dto/update-item.dto';
 
 @Injectable()
 export class ItemService {
@@ -12,7 +13,21 @@ export class ItemService {
   }
 
   async createItem(createItemDto: CreateItemDto) {
-    await this.itemModel.create(createItemDto);
+    const { expiresAt, ...rest } = createItemDto;
+    const date = new Date(expiresAt);
+    await this.itemModel.create({ expiresAt: date, ...rest });
     return { message: 'Item created', status: HttpStatus.CREATED };
+  }
+
+  async updateItem(updateItemDto: UpdateItemDto, id: string) {
+    const updatedItem = await this.itemModel.updateOne(
+      { _id: id },
+      updateItemDto,
+    );
+    return updatedItem;
+  }
+  async deleteItem(id: string) {
+    await this.itemModel.deleteOne({ _id: id });
+    return { message: 'Item Deleted', status: HttpStatus.OK };
   }
 }
