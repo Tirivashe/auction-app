ff30704fae5fb83a0d9b459e74c2893bd7f25645
Tirivashe <shaymusts@gmail.com>
2024-04-11 17:49:10 +0200

    testing out dates with the appropriate config

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+
+import "./App.css";
+import { DateTimePicker, DateValue } from "@mantine/dates";
+import dayjs from "dayjs";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState<DateValue>();
+
+  useEffect(() => {
+    const niceFormat = dayjs(value).format("DD MMM, YYYY hh:mm A");
+    const epochNumber = dayjs(niceFormat).valueOf();
+    console.log({ niceFormat, epochNumber });
+  }, [value]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <DateTimePicker
+        valueFormat="DD MMM, YYYY hh:mm A"
+        label="Pick date and time"
+        placeholder="Pick date and time"
+        value={value}
+        onChange={setValue}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
+import "@mantine/notifications/styles.css";
 
 import { MantineProvider } from "@mantine/core";
 
