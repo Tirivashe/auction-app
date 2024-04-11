c04b96edeb372257d9c55f13037bc4452737e414
Tirivashe <shaymusts@gmail.com>
2024-04-11 19:42:36 +0200

    added new prop for capturing user that won the item

--- apps/server/src/item/schema/item.schema.ts
+++ apps/server/src/item/schema/item.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document, HydratedDocument } from 'mongoose';
+import mongoose, { Document, HydratedDocument } from 'mongoose';
+import { User } from 'src/user/schema/user.schema';
 
 export type ItemDocument = HydratedDocument<Item>;
 
@@ -22,6 +23,9 @@ export class Item extends Document {
 
   @Prop({ default: true })
   isActive: boolean;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null })
+  awardedTo: User;
 }
 
 export const ItemSchema = SchemaFactory.createForClass(Item);
