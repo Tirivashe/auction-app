d269c30159f9b1a113ea073f8e403629f01ef7c1
Tirivashe <shaymusts@gmail.com>
2024-04-14 16:03:21 +0200

    changed bidding endpoints

--- apps/server/src/bid/bid.controller.ts
+++ apps/server/src/bid/bid.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { BidService } from './bid.service';
 
 @Controller()
@@ -8,5 +8,10 @@ export class BidController {
   @Get('bids')
   async getAllBids() {
     return await this.bidService.getAllBids();
+  }
+
+  @Get('bid/:id')
+  async getBidsByItemId(@Param('id') id: string) {
+    return await this.bidService.getBidsByItemId(id);
   }
 }
--- apps/server/src/bid/bid.service.ts
+++ apps/server/src/bid/bid.service.ts
@@ -5,9 +5,7 @@ import { InjectConnection, InjectModel } from '@nestjs/mongoose';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { BiddingHistory } from './schema/bid-history.schema';
 import { UserSettings } from 'src/user/schema/user-settings.schema';
-// import { BidEvents } from './events/bid-events';
 import { ItemService } from 'src/item/item.service';
-import { PlaceBidDto } from 'src/item/dto/place-bid.dto';
 
 @Injectable()
 export class BidService {
@@ -26,65 +24,9 @@ export class BidService {
     return await this.bidModel.find().populate('user').populate('item');
   }
 
-  // TODO: Make this run concurrently correctly and reduce complexity
-  // @OnEvent(BidEvents.CREATED, { async: true })
-  // async autobidForUser({
-  //   itemId,
-  //   placeBidDto,
-  // }: {
-  //   itemId: string;
-  //   placeBidDto: PlaceBidDto;
-  // }) {
-  //   const nextBidAmount = placeBidDto.amount + 1;
-  //   const userList = await this.usersWithAutobidEnabled({
-  //     itemId,
-  //     placeBidDto,
-  //   });
-  //   const session = await this.connection.startSession();
-  //   session.startTransaction();
-  //   try {
-  //     for (const user of userList) {
-  //       const settings: UserSettings = await this.userSettingsModel.findOne({
-  //         user: user.user,
-  //       });
-  //       if (
-  //         settings.maxBidAmount &&
-  //         settings.maxBidAmount > 0 &&
-  //         settings.totalAmountReserved + nextBidAmount > settings.maxBidAmount
-  //       )
-  //         continue;
-
-  //       await this.itemService.createBid(
-  //         itemId,
-  //         { userId: user._id.toString(), amount: nextBidAmount },
-  //         session,
-  //       );
-  //       const userSettings = await this.userSettingsModel.findOneAndUpdate(
-  //         { user: user.user },
-  //         { $inc: { totalAmountReserved: nextBidAmount } },
-  //         { new: true },
-  //       );
-
-  //       await userSettings.save({ session });
-
-  //       this.eventEmitter.emit(BidEvents.CREATED, {
-  //         itemId: itemId,
-  //         userId: user._id.toString(),
-  //         amount: nextBidAmount,
-  //       });
-  //     }
-  //     await session.commitTransaction();
-  //   } catch (err) {
-  //     await session.abortTransaction();
-  //   } finally {
-  //     session.endSession();
-  //   }
-  //   this.eventEmitter.emit(BidEvents.AUTO_BID_CREATED, {
-  //     itemId,
-  //     nextBidAmount,
-  //     placeBidDto,
-  //   });
-  // }
+  async getBidsByItemId(itemId: string): Promise<Bid[]> {
+    return await this.bidModel.find({ item: itemId }).populate('item');
+  }
 
   // @OnEvent(BidEvents.AUTO_BID_CREATED, { async: true })
   // async onAutobidCreated({
@@ -127,20 +69,20 @@ export class BidService {
   //   }
   // }
 
-  private async usersWithAutobidEnabled({
-    itemId,
-    placeBidDto,
-  }: {
-    itemId: string;
-    placeBidDto: PlaceBidDto;
-  }) {
-    const usersWithAutobidEnabled: BiddingHistory[] =
-      await this.biddingHistoryModel.find({
-        item: itemId,
-        autobid: true,
-        user: { $ne: placeBidDto.userId },
-      });
+  // private async usersWithAutobidEnabled({
+  //   itemId,
+  //   placeBidDto,
+  // }: {
+  //   itemId: string;
+  //   placeBidDto: PlaceBidDto;
+  // }) {
+  //   const usersWithAutobidEnabled: BiddingHistory[] =
+  //     await this.biddingHistoryModel.find({
+  //       item: itemId,
+  //       autobid: true,
+  //       user: { $ne: placeBidDto.userId },
+  //     });
 
-    return usersWithAutobidEnabled;
-  }
+  //   return usersWithAutobidEnabled;
+  // }
 }
