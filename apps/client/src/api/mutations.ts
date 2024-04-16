b3ea4677be0934fa0ac4b257ec37acecee0aac44
Tirivashe <shaymusts@gmail.com>
2024-04-16 15:26:03 +0200

    integration with axios instead of fetch

--- apps/client/src/api/mutations.ts
+++ apps/client/src/api/mutations.ts
@@ -1,17 +1,18 @@
-import { LoginDto, SignUpDto } from "../types";
+import { LoginDto, ServerAuthResponse, SignUpDto } from "../types";
+import axios, { type AxiosResponse } from "axios";
 
-export const signUp = (body: SignUpDto) => {
-  return fetch("/api/auth/signup", {
-    headers: { "Content-Type": "application/json" },
-    method: "POST",
-    body: JSON.stringify(body),
-  }).then((res) => res.json());
+export const signUp = async (body: SignUpDto) => {
+  const res: AxiosResponse<ServerAuthResponse> = await axios.post(
+    "/api/auth/signup",
+    body
+  );
+  return res.data;
 };
 
-export const login = (body: LoginDto) => {
-  return fetch("/api/auth/login", {
-    headers: { "Content-Type": "application/json" },
-    method: "POST",
-    body: JSON.stringify(body),
-  }).then((res) => res.json());
+export const login = async (body: LoginDto) => {
+  const res: AxiosResponse<ServerAuthResponse> = await axios.post(
+    "/api/auth/login",
+    body
+  );
+  return res.data;
 };
