81580aed6c9264fc2c8fd2a91c4f038017123b7b
Tirivashe <shaymusts@gmail.com>
2024-04-16 20:20:09 +0200

    added validation to create item dto

--- apps/server/src/item/dto/create-item.dto.ts
+++ apps/server/src/item/dto/create-item.dto.ts
@@ -1,7 +1,30 @@
+import {
+  IsDateString,
+  IsMimeType,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
 export class CreateItemDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
   price: number;
-  image: string;
+
+  @IsMimeType()
+  @IsOptional()
+  image?: string;
+
+  @IsDateString()
+  @IsNotEmpty()
   expiresAt: Date;
 }
