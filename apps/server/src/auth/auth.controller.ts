c58b3f7863d3808711e53953ed43049f0bb92fb3
Tirivashe <shaymusts@gmail.com>
2024-04-16 09:10:06 +0200

    updated return type of controller to match service

--- apps/server/src/auth/auth.controller.ts
+++ apps/server/src/auth/auth.controller.ts
@@ -2,18 +2,23 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/auth-signup.dto';
 import { LoginDto } from './dto/auth-login.dto';
+import { User } from 'src/user/schema/user.schema';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  async signup(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
+  async signup(
+    @Body() signUpDto: SignUpDto,
+  ): Promise<{ token: string; user: User }> {
     return await this.authService.signUp(signUpDto);
   }
 
   @Post('login')
-  async login(@Body() loginDto: LoginDto): Promise<{ token: string }> {
+  async login(
+    @Body() loginDto: LoginDto,
+  ): Promise<{ token: string; user: User }> {
     return await this.authService.login(loginDto);
   }
 }
