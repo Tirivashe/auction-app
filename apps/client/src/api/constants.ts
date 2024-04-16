2956085fdc57e5877b34bb6edbcfc41e973242e0
Tirivashe <shaymusts@gmail.com>
2024-04-16 16:59:15 +0200

    added axios instance

---
+++ apps/client/src/api/constants.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+const authStorageString = localStorage.getItem("auth-storage");
+const parseAuthStorage: { state: { token: string } } | null = authStorageString
+  ? JSON.parse(authStorageString)
+  : null;
+
+export const axiosInstance = axios.create({
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${parseAuthStorage?.state.token || ""}`,
+  },
+});
