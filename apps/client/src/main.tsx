2ef22b232ad63c03a206c0358bfe8ba515c045e2
Tirivashe <shaymusts@gmail.com>
2024-04-16 10:01:36 +0200

    style changes in main

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "./index.css";
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
 import "@mantine/notifications/styles.css";
