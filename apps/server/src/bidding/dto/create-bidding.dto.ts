565f3129bfb100b8c319a8381e9c8a9fdb0c35e4
Tirivashe <shaymusts@gmail.com>
2024-04-17 21:03:27 +0200

    validation for bidding params

--- apps/server/src/bidding/dto/create-bidding.dto.ts
+++ apps/server/src/bidding/dto/create-bidding.dto.ts
@@ -1,6 +1,25 @@
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
 export class CreateBiddingDto {
+  @IsNotEmpty()
+  @IsString()
   itemId: string;
+
+  @IsNotEmpty()
+  @IsString()
   userId: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   amount: number;
+
+  @IsOptional()
+  @IsBoolean()
   autobid?: boolean;
 }
