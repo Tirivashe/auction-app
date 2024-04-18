3aea2f4d184ea7c540a9b6cbc829226e1cf97be5
Tirivashe <shaymusts@gmail.com>
2024-04-18 09:14:26 +0200

    navigation to user profile page

--- apps/client/src/components/Header/index.tsx
+++ apps/client/src/components/Header/index.tsx
@@ -1,10 +1,15 @@
-import { Button, Group } from "@mantine/core";
+import { Avatar, Button, Group } from "@mantine/core";
 import { useAuthStore } from "../../store";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const logout = useAuthStore((state) => state.logout);
+  const user = useAuthStore((state) => state.user);
   return (
     <Group h="inherit" justify="flex-end" px="lg">
+      <Avatar color="blue" radius="xl" component={Link} to="/profile">
+        {user?.username.charAt(0).toUpperCase()}
+      </Avatar>
       <Button onClick={logout}>Logout</Button>
     </Group>
   );
