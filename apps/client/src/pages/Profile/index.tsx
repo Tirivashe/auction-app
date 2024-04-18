864753a221824ff0aa590142ca86c3cb5284f176
Tirivashe <shaymusts@gmail.com>
2024-04-18 23:08:12 +0200

    filtering null item values

--- apps/client/src/pages/Profile/index.tsx
+++ apps/client/src/pages/Profile/index.tsx
@@ -48,36 +48,37 @@ const ProfilePage = () => {
     open();
   };
 
-  const rows = items.map((row) => {
-    const color =
-      row.bidStatus === BidStatus.Won
-        ? "green"
-        : row.bidStatus === BidStatus.Lost
-          ? "red"
-          : "orange";
-    return (
-      <Table.Tr key={row._id}>
-        <Table.Td>{row?.item?.name}</Table.Td>
-        <Table.Td>${row?.item?.price}</Table.Td>
-        <Table.Td>
-          <Badge color={color}>
-            {row?.bidStatus === BidStatus.InProgress
-              ? "In Progress"
-              : row.bidStatus}
-          </Badge>
-        </Table.Td>
-        <Table.Td>{formatTime(row?.createdAt)}</Table.Td>
-        <Table.Td>{formatTime(row?.item.expiresAt)}</Table.Td>
-        {row.bidStatus === BidStatus.Won && (
+  const rows = items
+    .filter((row) => row.item !== null)
+    .map((row) => {
+      const color =
+        row.bidStatus === BidStatus.Won
+          ? "green"
+          : row.bidStatus === BidStatus.Lost
+            ? "red"
+            : "orange";
+      return (
+        <Table.Tr key={row._id} py="lg" h="60px">
+          <Table.Td>{row?.item?.name}</Table.Td>
+          <Table.Td>${row?.item?.price}</Table.Td>
           <Table.Td>
-            <ActionIcon p="2px" onClick={() => openBill(row.item._id)}>
-              <IconFileInvoice size="3.5rem" />
-            </ActionIcon>
+            <Badge color={color}>
+              {row?.bidStatus === BidStatus.InProgress
+                ? "In Progress"
+                : row.bidStatus}
+            </Badge>
           </Table.Td>
-        )}
-      </Table.Tr>
-    );
-  });
+          <Table.Td>{formatTime(row?.item?.expiresAt)}</Table.Td>
+          {row.bidStatus === BidStatus.Won && (
+            <Table.Td>
+              <ActionIcon p="2px" onClick={() => openBill(row.item._id)}>
+                <IconFileInvoice size="3.5rem" />
+              </ActionIcon>
+            </Table.Td>
+          )}
+        </Table.Tr>
+      );
+    });
 
   return (
     <Container m="xl" maw="100%">
@@ -90,7 +91,6 @@ const ProfilePage = () => {
               <Table.Th>Item Name</Table.Th>
               <Table.Th>Item Price</Table.Th>
               <Table.Th>Status</Table.Th>
-              <Table.Th>Bid Started On</Table.Th>
               <Table.Th>Closes At</Table.Th>
             </Table.Tr>
           </Table.Thead>
