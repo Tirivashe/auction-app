67d4897d1aeccb87ee526d4363a7d6cb05343839
Tirivashe <shaymusts@gmail.com>
2024-04-19 14:39:47 +0200

    add notifications

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Notifications } from "@mantine/notifications";
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
 import "@mantine/notifications/styles.css";
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <MantineProvider>
       <WebsocketProvider value={socket}>
         <QueryClientProvider client={queryClient}>
+          <Notifications limit={1} />
           <App />
         </QueryClientProvider>
       </WebsocketProvider>
