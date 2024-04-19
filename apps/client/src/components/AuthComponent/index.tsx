e8c25bad1cb1d79ba9019dd1b54513d6c7ff11c8
Tirivashe <shaymusts@gmail.com>
2024-04-19 10:08:30 +0200

    default to login screen

--- apps/client/src/components/AuthComponent/index.tsx
+++ apps/client/src/components/AuthComponent/index.tsx
@@ -4,7 +4,7 @@ import { Login } from "./Login";
 import { AuthForm } from "../../types";
 
 const AuthComponent = () => {
-  const [authForm, setAuthForm] = useState<AuthForm>(AuthForm.SIGNUP);
+  const [authForm, setAuthForm] = useState<AuthForm>(AuthForm.LOGIN);
   const changeAuthForm = (form: AuthForm) => setAuthForm(form);
   const displayedAuthForm = {
     [AuthForm.SIGNUP]: <SignUp changeForm={changeAuthForm} />,
